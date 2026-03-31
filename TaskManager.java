7220d7ee4e25cce0f6b2f522152143a58dffe2df
AdityaJain-05 <aj6762064@gmail.com>
2026-03-31 22:25:49 +0530

    Update TaskManager.java

--- TaskManager.java
+++ TaskManager.java
@@ -1,1 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class TaskManager {
+
+    static ArrayList<Task> tasks = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n--- Student Task Manager ---");
+            System.out.println("1. Add Task");
+            System.out.println("2. View Tasks");
+            System.out.println("3. Mark Task Completed");
+            System.out.println("4. Exit");
+
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("Enter task title: ");
+                    String title = sc.nextLine();
+                    tasks.add(new Task(title));
+                    break;
+
+                case 2:
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(i + ". " + tasks.get(i));
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Enter task number: ");
+                    int index = sc.nextInt();
+                    tasks.get(index).markCompleted();
+                    break;
+            }
+
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
