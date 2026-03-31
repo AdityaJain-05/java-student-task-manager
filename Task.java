0076c8fdd3808165f9ec49d8aa47f56c04f33098
AdityaJain-05 <aj6762064@gmail.com>
2026-03-31 22:25:25 +0530

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,1 +1,17 @@
+public class Task {
+    String title;
+    boolean completed;
 
+    public Task(String title) {
+        this.title = title;
+        this.completed = false;
+    }
+
+    public void markCompleted() {
+        completed = true;
+    }
+
+    public String toString() {
+        return title + " - " + (completed ? "Completed" : "Pending");
+    }
+}
